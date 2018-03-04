d2dd8c37cb633069b333379b5cf589c92b60c65f
elias-alea <elias.kenyabusiness@gmail.com>
2018-03-04 15:54:07 +0300

    lab 1 assignment - csv to json

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+const csvToJson = require('csvtojson')
+const csvFilePath = 'customer-data.csv'
+const customerStr = '['
+
+csvToJson()
+    .fromFile(csvFilePath, {
+        toArrayString: true
+    })
+    .on('data', (data) => {
+        // concatentate strigified JSON here
+        customerStr += data.toString('utf8') + ','
+    }).on('end', ()=> {
+        // Remove extra comma from JSON string and add closing array square bracket
+        customerStr = customerStr.substring(0, customerStr.length - 1)
+        customerStr += ']'
+    })
+    .on('error', (error) => {
+        console.log(`There was an error processing your file. See below \n ${error.message}`)
+    })
+    .on('done', ()=> {
+        fs.writeFileSync(path.join(__dirname, 'customer-data.json'), customerStr)
+    })
+    
